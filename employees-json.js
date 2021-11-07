bbb22a26b2369faf145b54f0b255ea5c6de46820
eZharikov <64029634+eZharikov@users.noreply.github.com>
2021-11-07 13:43:45 +0300

    1

---
+++ employees-json.js
@@ -0,0 +1,35 @@
+const DATA = {
+
+    employees: [
+        {
+            id: 1,
+            name: "Пафнутий",
+            surname: "Пафнутьев",
+            department: "IT"
+        },
+        {
+            id: 2,
+            name: "Иван",
+            surname: "Иванов",
+            department: "marketing"
+        },
+        {
+            id: 3,
+            name: "Петр",
+            surname: "Петров",
+            department: "finance"
+        },
+        {
+            id: 4,
+            name: "Василий",
+            surname: "Васильев",
+            department: "IT"
+        },
+        {
+            id: 5,
+            name: "Антон",
+            surname: "Антонов",
+            department: "management"
+        }
+    ]
+}
